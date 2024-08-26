27415e4a89874f20eabf51c6d948be1654c9d598
Misbah sajjad <154726572+misbahs10@users.noreply.github.com>
2024-08-26 15:28:40 +0500

    Delete CHALK directory

--- CHALK/chalk.js
+++
@@ -1,9 +0,0 @@
-import inquirer from "inquirer4";
-import chalk from "chalk";
-let answers = await inquirer.prompt([{
-        name: "age",
-        type: "number",
-        message: "Enter your Age:"
-    }
-]);
-console.log(chalk.blue("Insha Allah, in " + (60 - answers.age) + " years you will be 60 years old."));
