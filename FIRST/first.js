47ab30ec9c637ab1595b38308d3ad4ed0ff30882
Misbah sajjad <154726572+misbahs10@users.noreply.github.com>
2024-08-26 15:34:49 +0500

    Delete FIRST directory

--- FIRST/first.js
+++
@@ -1,8 +0,0 @@
-var name1 = "'Misbah ";
-var fathername1 = "Sajjad'";
-console.log("My name is ".concat(name1 + fathername1));
-console.log("---------------------");
-var intro = "My name is ";
-var name2 = "(Misbah ";
-var fathername2 = "Sajjad)";
-console.log(intro + name2 + fathername2);
