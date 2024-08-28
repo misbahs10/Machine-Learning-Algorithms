8f1b3923fd1c1ad2a8c32720d43480367112c86d
Misbah sajjad <154726572+misbahs10@users.noreply.github.com>
2024-08-28 16:07:57 +0500

    Add files via upload

---
+++ python/veriables and datatypes/sum.py
@@ -0,0 +1,6 @@
+
+num1 = 46
+num2 = 89
+Sum = num1 + num2
+print(Sum)
+
---
+++ python/veriables and datatypes/variable.py
@@ -0,0 +1,8 @@
+
+# variable mean jis ki value change ho jay
+
+name  = "misbah",
+age = 18,
+price = 30.4,
+
+print(name + age + price)
