b22d5c84897e80d2ddf3d7a7cb0b5a6aae097763
Misbah sajjad <154726572+misbahs10@users.noreply.github.com>
2024-08-30 12:55:48 +0500

    Delete python directory

--- python/character-set.py
+++
@@ -1,7 +0,0 @@
-
-#               PYTHON CHARACTER SET
-
-# 1. letters - A to Z
-# 2. digits - 0 to 9
-# 3. special symbol _ + , - , * , / ,etc
-# 4. write space - blank space, tab, carriage return, newline, formfeed
